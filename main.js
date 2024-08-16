6418295ac0bbb4e93e1ecb4ac3519c06bd64d23d
Ben <bengeraghty.bg@gmail.com>
2024-08-16 15:59:24 +0700

    finish basic function remove task items

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
-console.log('Testing line for JavaScript 👽👽👽👽');
-
 class Task {
     constructor(description, labelColor){
         this.description = description;
         this.labelColor = labelColor;
     }
 
-    get printMessage(){
+    printMessage(){
         console.log(`You added '${this.description}', this task has a ${this.labelColor} priority.`)
     }
+
+    formatTask(){
+        return ` <div style="background-color:${this.labelColor}" class="list-item">
+              <p class="list-item__text">${this.description}</p>
+              <button class="btn-remove">✖</button>
+            </div>`
+    }
+    
    
 }
-
-const shop = new Task('buy groceries','red');
-const organize = new Task('organize desk','green');
-console.log(shop, organize);
-shop.printMessage;
-organize.printMessage;
-
 const addButton = document.getElementById('btn-add');
 const taskField = document.getElementById('task-field');
 const listItems = [];
@@ -29,16 +28,68 @@ taskField.addEventListener('keydown', (e) => {
     }
 });
 
+
 function addTask() {
     if (taskField.value.trim() === ''){
         console.log('Please enter something');
     } else {
         const fieldValue = taskField.value;
-        listItems.push(fieldValue);
+        const priorityColor = 'crimson'; // set as default for now
+        const newTask = new Task(fieldValue, priorityColor); 
+        listItems.push(newTask);
+        newTask.printMessage;        
         taskField.value = '';
-        console.log(listItems);
+        
+        updateList();
+        
+        
     }
 }
+
+function updateList(){
+    const taskList = document.getElementById('task-list');
+    taskList.innerHTML = '';
+   listItems.forEach(task => {
+        const taskHTML = task.formatTask();
+        taskList.innerHTML += taskHTML;
+
+        getDeleteButtons();
+    });
+}
+
+function crossOutTask(){
+
+}
+
+function removeTask(e){
+  console.log('task removed');
+  const taskItemTarget = e.target;
+  const taskItem = taskItemTarget.closest('.list-item');
+//   taskItem.remove();
+
+if (taskItem) {
+   console.log('Task item exists', taskItem);
+
+   const parentEl = taskItem.parentElement;
+  if (parentEl) {
+    const taskIndex = Array.from(parentEl.children).indexOf(taskItem);
+            console.log('Index position:', taskIndex);
+
+            listItems.splice(taskIndex, 1);
+
+            taskItem.remove();
+  }
+}
+
+}
+
+function getDeleteButtons(){
+    const removeButtons = document.querySelectorAll('.btn-remove');
+    removeButtons.forEach(button => {
+    button.addEventListener('click', removeTask);
+})
+}
+
 
 /*
 Red - Crucial (Very Important)
